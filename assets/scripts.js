fc73d9bf2b238afd2834017df3db98517ff3f039
explodingcamera <explodingcamera@users.noreply.github.com>
2022-05-26 21:46:50 +0000

    deploy: 08b5ccf8e03c1577864662d7efb264f127ec6057

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,12 +1,93 @@
-import barba from "https://cdn.skypack.dev/@barba/core@2.9.7";
+import barba from "https://cdn.skypack.dev/@barba/core@v2.9.7";
+import butterchurn from "https://cdn.skypack.dev/butterchurn@v2.6.7";
+import butterchurnPresets from "https://cdn.skypack.dev/butterchurn-presets@v2.4.7";
 
 barba.init({});
 
-let audio = document.createElement("audio");
-audio.volume = 0.1;
-audio.loop = true;
-audio.src = "/assets/sound/lounge.ogg";
-audio.autoplay = true;
-document.body.appendChild(audio);
+const audioEl = playSong();
+const [audioCtx, audioNode] = createAudioContext(audioEl);
+const visualizer = setupAudioVisualizer(audioCtx, audioNode);
+connectToAudioAnalyzer(visualizer, audioCtx, audioNode);
 
-document.onclick = () => audio.play();
+window.playSong = playSong;
+
+window.addEventListener(
+  "resize",
+  debounce(() =>
+    visualizer.setRendererSize(window.innerWidth, window.innerHeight)
+  )
+);
+
+window.addEventListener("click", () => {
+  // check if context is in suspended state (autoplay policy)
+  if (audioCtx.state === "suspended") audioCtx.resume();
+
+  audioEl.play();
+});
+
+function debounce(func) {
+  var timer;
+  return function (event) {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(func, 100, event);
+  };
+}
+
+function playSong(song = "/assets/sound/lounge.ogg") {
+  let audioEl = document.querySelector("#music");
+  audioEl.volume = 0.1;
+  audioEl.loop = true;
+  audioEl.src = song;
+  audioEl.play();
+  return audioEl;
+}
+
+function createAudioContext(audioEl) {
+  const audioCtx = new AudioContext();
+  const node = audioCtx.createMediaElementSource(audioEl);
+  node.connect(audioCtx.destination);
+  return [audioCtx, node];
+}
+
+function setupAudioVisualizer(audioCtx, audioNode) {
+  const canvas = document.querySelector("#background");
+
+  const visualizer = butterchurn.createVisualizer(audioCtx, canvas, {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  const presets = butterchurnPresets.getPresets();
+  const preset =
+    presets[
+      Object.keys(presets)[
+        Math.floor(Math.random() * Object.keys(presets).length)
+      ]
+    ];
+
+  console.log(
+    Object.keys(presets)[
+      Math.floor(Math.random() * Object.keys(presets).length)
+    ]
+  );
+
+  visualizer.loadPreset(preset, 0.0);
+  window.requestAnimationFrame(loop);
+
+  function loop() {
+    visualizer.render();
+    window.requestAnimationFrame(loop);
+  }
+
+  return visualizer;
+}
+
+function connectToAudioAnalyzer(visualizer, audioCtx, sourceNode) {
+  const gainNode = audioCtx.createGain();
+  const biquadFilter = audioCtx.createBiquadFilter();
+  gainNode.gain.value = 2;
+  sourceNode.connect(gainNode);
+  gainNode.connect(biquadFilter);
+
+  visualizer.connectAudio(biquadFilter);
+}
