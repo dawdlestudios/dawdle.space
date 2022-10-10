33a7848fdde5b442127b2c337a5d39300a063595
Henry Gressmann <mail@henrygressmann.de>
2022-10-10 21:02:47 +0200

    add wallpapers
    Signed-off-by: Henry Gressmann <mail@henrygressmann.de>

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,10 @@ module.exports = function (eleventyConfig) {
   // Copy `assets/` to `_site/assets`
   eleventyConfig.addPassthroughCopy("assets");
 
+  eleventyConfig.addHandlebarsHelper("image", (title, image_url) =>
+    `<a href=${image_url}><figure><img src="${image_url}" alt="${title}"><figcaption>${title}</figcaption></figure></a>`)
+  ;
+
   return {
     dir: {
       input: "pages",
