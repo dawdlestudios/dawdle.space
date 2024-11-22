442b6f147fd7e36cadb8b97547dfa9865a35a373
Henry Gressmann <mail@henrygressmann.de>
2024-11-22 21:14:48 +0100

    chore: update comrak
    Signed-off-by: Henry Gressmann <mail@henrygressmann.de>

--- src/ssg/markdown.rs
+++ src/ssg/markdown.rs
@@ -1,26 +1,28 @@
 use comrak::{
-    plugins::syntect::SyntectAdapterBuilder, ExtensionOptionsBuilder, ParseOptionsBuilder, Plugins,
-    RenderOptionsBuilder,
+    plugins::syntect::SyntectAdapterBuilder, ExtensionOptions, ParseOptions, Plugins,
+    RenderOptions, RenderPlugins,
 };
 
 pub fn md_to_html(buf: &str) -> String {
     let options = comrak::ComrakOptions {
-        parse: ParseOptionsBuilder::default().build().unwrap(),
-        render: RenderOptionsBuilder::default()
-            .unsafe_(true)
-            .build()
-            .unwrap(),
-        extension: ExtensionOptionsBuilder::default()
-            .front_matter_delimiter(Some("---".to_string()))
-            .build()
-            .unwrap(),
+        parse: ParseOptions::default(),
+        render: RenderOptions::builder().unsafe_(true).build(),
+        extension: ExtensionOptions::builder()
+            .front_matter_delimiter("---".to_string())
+            .build(),
     };
 
-    let mut plugins = Plugins::default();
     let syntax_highlighter = SyntectAdapterBuilder::default()
         .theme("base16-ocean.dark")
         .build();
-    plugins.render.codefence_syntax_highlighter = Some(&syntax_highlighter);
 
-    comrak::markdown_to_html_with_plugins(buf, &options, &plugins)
+    let renderer = RenderPlugins::builder()
+        .codefence_syntax_highlighter(&syntax_highlighter)
+        .build();
+
+    comrak::markdown_to_html_with_plugins(
+        buf,
+        &options,
+        &Plugins::builder().render(renderer).build(),
+    )
 }
